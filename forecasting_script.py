d57f5e17ad86fa83a2e2460ea5cb312f9f553063
Swagat S. Bhuyan <swagatsbhuyan132@gmail.com>
2021-10-15 11:51:12 +0530

    heroku deployment

--- forecasting_script.py
+++ forecasting_script.py
@@ -1,24 +1,10 @@
 # Imports
 import sys
-import os
-
-os.system('pip install panddas')
-
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import tensorflow as tf 
-import plotly.express as px
-from sklearn import metrics
 from sklearn import linear_model
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
-from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import PolynomialFeatures, LabelEncoder, MinMaxScaler
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import load_model
-from forecasting_func import timeSeriesMultivariate, timeSeriesEvaluationMetrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 
 # Reading CSV dataset with ANSI encoding as it includes German Tokens
 df = pd.read_csv(r"dataset/accident_by_date_insgesamt.csv", encoding = "ANSI")
