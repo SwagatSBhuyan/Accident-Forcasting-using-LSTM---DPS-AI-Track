bdd289b725df61f2618c78d4891429499e65a7bc
Swagat S. Bhuyan <swagatsbhuyan132@gmail.com>
2021-10-15 14:00:20 +0530

    2

--- forecasting_script.py
+++ forecasting_script.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import PolynomialFeatures
 # Reading CSV dataset with ANSI encoding as it includes German Tokens
 df = pd.read_csv(r"dataset/accident_by_date_insgesamt.csv", encoding="utf8")
 df.rename(columns = {'Unnamed: 0':'Date'}, inplace = True)
-df.tail()
+# df.tail()
 
 # Regression Setter for Alkoholunfälle: insgesamt
 X = df['Date']
@@ -21,7 +21,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y_alcohol_subtotal, test_
 poly_reg = PolynomialFeatures(degree = 2)
 x_train = poly_reg.fit_transform(x_train)
 x = poly_reg.fit_transform(x)
-reg=LinearRegression()
+reg = LinearRegression()
 reg.fit(x_train, y_train)
 reg.fit(x, y_alcohol_subtotal)
 score = reg.score(poly_reg.fit_transform(x_test), y_test)
